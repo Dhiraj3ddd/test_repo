bc8e93c5136b412cf5ae49cd0106519028e62c99
Dhiraj3ddd <deore.dhiraj2@gmail.com>
2025-01-01 23:11:00 +0530

    simple java add code

---
+++ JavaAdd.java
@@ -0,0 +1,10 @@
+package one;
+
+public class JavaAdd {
+    public static void main(String args[]){
+    int a= 5;
+    int b= 15;
+    int result=a+b;
+    System.out.println("a+b = " + result);
+  }
+}
