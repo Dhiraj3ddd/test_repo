5affea7d9f13024f4a7c123adcd6bce968f3080d
Dhiraj3ddd <deore.dhiraj2@gmail.com>
2025-01-04 23:42:14 +0530

    made calc java more user intuitive

--- Calculator.java
+++ Calculator.java
@@ -6,23 +6,24 @@ public class Calculator {
     public static void main (String args[]){
         Scanner sc = new Scanner(System.in);
 
-        
+        System.out.println("Enter number a ->" );
         int a = sc.nextInt();
         
+        System.out.println("Enter number b -> ");
         int b = sc.nextInt();
         
         char operator = sc.next().charAt(0);
 
         switch (operator){
-            case '+' : System.out.println(a+b);
+            case '+' : System.out.println("Result -> " + (a+b));
                        break;
-            case '-' : System.out.println(a-b);
+            case '-' : System.out.println("Result -> " + (a-b));
                        break;
-            case '*' : System.out.println(a*b);
+            case '*' : System.out.println("Result -> " + (a*b));
                        break;
-            case '/' : System.out.println(a/b);
+            case '/' : System.out.println("Result -> " + (a/b));
                        break;
-            case '%' : System.out.println(a%b);
+            case '%' : System.out.println("Result -> " + (a%b));
                        break;
 
             default : System.out.println("Unexpected input , please provide valid input ");
