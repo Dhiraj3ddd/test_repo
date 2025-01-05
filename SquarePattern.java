c277ee0580f30a32de1181255682fd2c83cbfb12
Dhiraj3ddd <deore.dhiraj2@gmail.com>
2025-01-05 23:56:04 +0530

    added while loop impl to same

--- SquarePattern.java
+++ SquarePattern.java
@@ -1,12 +1,34 @@
 package one;
 
-public class SquarePattern {
-    public static void main (String args[]){
-
+public class SquarePattern
+{
+    public static void main (String args[])
+    {
+        {
+        System.out.println (" Printing with FOR loop ") ; 
         for(int line = 1;
                 line<= 4;
-                line ++   ) {
+                line ++   ) 
+            {
             System.out.println (" *  *  *  * ") ;       
             }
         }
+    
+        {
+        int l =1;
+        System.out.println (" Printing with WHILE loop ") ;    
+
+        while (l <= 4 )
+           {
+           
+
+            System.out.println (" *  *  *  * ") ;
+            l++;
+           }
+        }
     }
+}
+
+    
+
+       
