0846578199ff6c12ba05108e3c78e15c708e124e
Dhiraj Deore <43905932+Dhiraj3ddd@users.noreply.github.com>
2025-01-07 23:29:13 +0530

    Update Pattern1.java

--- Pattern1.java
+++ Pattern1.java
@@ -4,7 +4,7 @@ public class Pattern1 {
 
     public static void main(String args[]){
         
-        for(int line=1;line <= 4 ;line++){
+        for(int line=1;line <= 5 ;line++){
             for(int star=1;star <= line;star++){
                 System.out.print("* ");
             }
