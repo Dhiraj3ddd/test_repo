0bcdb631ef757fd5cab2cf74513b2da903a329a7
Dhiraj3ddd <deore.dhiraj2@gmail.com>
2025-01-13 23:32:12 +0530

    minLengthLeetcode

---
+++ leetcode1.java
@@ -0,0 +1,37 @@
+package one;
+
+public class leetcode1 {
+    public int minLength(String s){
+    int [] freq = new int [26];
+    
+
+    for (char c : s.toCharArray()){
+        freq[c - 'a']++;
+       }
+    
+    
+
+    int length = 0;
+
+    for (int count : freq ){
+        if(count % 2 == 1){
+            length += 1;
+        } else {
+            length += Math.min(2,count);
+        }
+        }
+        return length ;
+       }
+   
+ 
+   public static void main(String args[]){
+
+    String s1 = "aaabbbccd";
+
+    leetcode1 sol = new leetcode1();
+    int minL = sol.minLength(s1);
+    System.out.println("Minimum length for \""+ s1 + "\" : "+ minL );
+   }
+}
+    
+
