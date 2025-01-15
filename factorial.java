4f4219797ef6e66d4c71b69120c5b955318762e1
Dhiraj3ddd <deore.dhiraj2@gmail.com>
2025-01-15 23:21:21 +0530

    factorial java

---
+++ factorial.java
@@ -0,0 +1,19 @@
+package one;
+
+public class factorial {
+    public static int facto (int n){
+
+        int f = 1;
+
+        for (int i =1 ; i <= n ; i++){
+
+            f = f * i;
+
+        } return f;
+
+    }
+
+    public static void main (String args[]){
+        System.out.println("The fatorial : " + facto(4));
+    }
+}
