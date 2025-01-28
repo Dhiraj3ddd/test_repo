c1d99cd66bc7b63acc111b41c7efd11427cdb5a2
Dhiraj Deore <43905932+Dhiraj3ddd@users.noreply.github.com>
2025-01-28 23:09:06 +0530

    Update JavaElse.java

--- JavaElse.java
+++ JavaElse.java
@@ -14,7 +14,7 @@ public static void main(String args[]) {
             System.out.println("Adult");
         }
         else
-        System.out.println("Not an adult");
+        System.out.println("Not adult");
 
     } catch (InputMismatchException e) {
         System.out.println("Invalid input. Please enter an integer.");
