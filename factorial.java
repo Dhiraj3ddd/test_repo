2145fbe98aa0a2b1c9271fd1854d0d5cfc65b434
Dhiraj Deore <43905932+Dhiraj3ddd@users.noreply.github.com>
2025-02-08 23:09:58 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -14,7 +14,7 @@ public class factorial {
     }
 
     public static void main (String args[]){
-        System.out.println("The fatorial is ->" + facto(7));
+        System.out.println("The fatorial is ->" + facto(6));
     }
 }
 
