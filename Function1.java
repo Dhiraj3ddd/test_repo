2c96fd297bd8d10c9fea186ac48ce7c4c23a1a8a
Dhiraj Deore <43905932+Dhiraj3ddd@users.noreply.github.com>
2025-12-16 17:12:27 +0530

    Update Function1.java

--- Function1.java
+++ Function1.java
@@ -1,8 +1,8 @@
-package one;
+package one ;
 
-import java.util.Scanner;
+import java.util.Scanner ;
 
-public class Function1 {
+public class Function1  {
    public static int calculateSum(int num1 , int num2){
     int sum = num1 + num2;
     return sum;
