15ec9bb074719496693da0504400d37782b1dc17
Dhiraj Deore <43905932+Dhiraj3ddd@users.noreply.github.com>
2025-12-18 22:47:29 +0530

    Update SquarePattern.java

--- SquarePattern.java
+++ SquarePattern.java
@@ -1,8 +1,8 @@
-package one;
+package one ;
 
 public class SquarePattern
 {
-    public static void main (String args[])
+    public static void main(String args[])
     {
         {
         System.out.println (" Printing with FOR loop "); 
