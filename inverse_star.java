e8aa7a5bd7936526e31440e31b0910ddeab8bc67
Dhiraj Deore <43905932+Dhiraj3ddd@users.noreply.github.com>
2025-12-19 23:52:28 +0530

    Update inverse_star.java

--- inverse_star.java
+++ inverse_star.java
@@ -1,6 +1,6 @@
-package one;
+package one ;
 
-public class inverse_star{
+public class inverse_star {
     public static void main(String args[]){
         int n =4 ;
         for(int line = 1; line <= n; line ++){
