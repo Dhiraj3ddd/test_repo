db6b383ed49efd5d11339f021123962c7d2ed201
Dhiraj Deore <43905932+Dhiraj3ddd@users.noreply.github.com>
2025-12-21 23:23:29 +0530

    Fix formatting and package declaration in Firstcode.java

--- Firstcode.java
+++ Firstcode.java
@@ -1,5 +1,5 @@
-package one ;
-public class Firstcode {
+package one;
+public class Firstcode{
     public static void main (String args[]){
 
         System.out.println("This is a java statement");
