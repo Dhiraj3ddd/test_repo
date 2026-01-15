c8c9291c5cfa1be268cb334f9c506d5b6158bc57
Dhiraj Deore <43905932+Dhiraj3ddd@users.noreply.github.com>
2026-01-15 22:14:57 +0530

    Update primeJava.java

--- primeJava.java
+++ primeJava.java
@@ -3,7 +3,7 @@ package one;
 import java.util.Scanner ;
 
 public class primeJava {
-    public static void main (String args[])
+    public static void main  (String args[])
     {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter value of int n -> ");
