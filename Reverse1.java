5ccf9fc2e5bc1fa6edbda3e1e65448d7195a2b8a
Dhiraj Deore <43905932+Dhiraj3ddd@users.noreply.github.com>
2026-01-25 22:50:39 +0530

    Update Reverse1.java

--- Reverse1.java
+++ Reverse1.java
@@ -1,16 +1,16 @@
-package one;
+package one ;
 
 public class Reverse1 
 {
     public static void main(String args[])
     {
-        int a = 1892923456;
+        int a =1892923456;
 
         while (a>0)
         {
 
-            int lastDigit = a%10;
-            System.out.print (lastDigit) ;
+            int lastDigit = a% 10;
+            System.out.print ( lastDigit) ;
             a/= 10;
 
         }
