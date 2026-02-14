fe314c849d3de502bf9640c1852f47e665757d36
Dhiraj Deore <43905932+Dhiraj3ddd@users.noreply.github.com>
2026-02-14 20:44:10 +0530

    Update JavaAdd.java

--- JavaAdd.java
+++ JavaAdd.java
@@ -6,10 +6,10 @@ public class JavaAdd {
     public static void main(String args[]){
       Scanner sc = new Scanner( System.in);
       
-      System.out.println("Enter number a->" );
+      System.out.println("Enter number a ->" );
       int a = sc.nextInt();
       
-      System.out.println("Enter number b-> ");
+      System.out.println("Enter number b -> ");
       int b = sc.nextInt();
     
     int result = a + b ;
