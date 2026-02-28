dc952aaa08b5ddb7c333b7803232266afcdb6cdf
Dhiraj Deore <43905932+Dhiraj3ddd@users.noreply.github.com>
2026-02-28 22:54:00 +0530

    Update JavaAdd.java

--- JavaAdd.java
+++ JavaAdd.java
@@ -9,7 +9,7 @@ public class JavaAdd {
       System.out.println("Enter number a ->" );
       int a = sc.nextInt();
       
-      System.out.println("Enter number b -> ");
+      System.out.println("Enter integer b -> ");
       int b = sc.nextInt();
     
     int result = a + b ;
