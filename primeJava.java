b225f7ac3c7b2b6218b9aaa14a6c4c8d2465afad
Dhiraj Deore <43905932+Dhiraj3ddd@users.noreply.github.com>
2026-04-09 00:02:05 +0530

    Update primeJava.java

--- primeJava.java
+++ primeJava.java
@@ -11,7 +11,7 @@ public class primeJava {
         
 
         if (n ==2 ){
-            System.out.println("n is prime number ");
+            System.out.println("n is a prime number ");
         }
         else 
         {
